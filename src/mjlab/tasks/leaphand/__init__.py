feb03d8a4c89ff8840c951d3069f7c41546a77b7
ferry <kenny0326@qq.com>
2026-03-30 17:30:17 +0800

    First commit

--- src/mjlab/tasks/leaphand/__init__.py
+++ src/mjlab/tasks/leaphand/__init__.py
@@ -3,6 +3,11 @@ from mjlab.tasks.leaphand.leaphand_finger_env_cfg import (
     LeapHandControlCfg,
 )
 
+from mjlab.tasks.leaphand.leaphand_palm_env_cfg import (
+    leaphand_palm_contact_env_cfg,
+    LeapHandPalmControlCfg,
+)
+
 from mjlab.tasks.registry import register_mjlab_task
 
 register_mjlab_task(
@@ -10,4 +15,11 @@ register_mjlab_task(
   env_cfg=leaphand_contact_env_cfg(),
   play_env_cfg=leaphand_contact_env_cfg(play=True),
   rl_cfg=LeapHandControlCfg(amplitude=0.8), 
+)
+
+register_mjlab_task(
+  task_id="Leaphand-Palm-Compliance-Control",
+  env_cfg=leaphand_palm_contact_env_cfg(),
+  play_env_cfg=leaphand_palm_contact_env_cfg(play=True),
+  rl_cfg=LeapHandPalmControlCfg(amplitude=0.8),
 )
